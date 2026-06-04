5e7d0b16e564e5c8dd0be87cceada3859b37d59c
codn0185 <113341200+codn0185@users.noreply.github.com>
2026-06-05 02:42:24 +0900

    refactor: 플레이어 구조체 생성하여 관련 정보 관리

--- src/ar_shuffleboard/models/shuffleboard.py
+++ src/ar_shuffleboard/models/shuffleboard.py
@@ -22,32 +22,40 @@ SCORE_AREA_LIST: list[ScoreArea] = [  # 점수 영역 데이터 리스트 (점�
 
 
 @dataclass
-class Player:
-    id: int
-    name: str
-    score: int
-    puck_color: tuple[int, int, int]  # bgr
-
-
-PUCK_COLORS: list[tuple[int, int, int]] = [  # bgr
-    (255, 0, 0),  # blue
-    (0, 0, 255),  # red
-    (0, 255, 0),  # green
-    (0, 255, 255),  # yellow
-]
+class Puck:
+    body: pymunk.Body
+    shape: pymunk.Shape
 
 
 @dataclass
-class Puck:
-    owner: Player
-    body: pymunk.Body
-    shape: pymunk.Shape
+class PlayerData:
+    name: str
+    color: tuple[int, int, int, int]  # BGRA
+    total_pucks: int
+    left_pucks: int
+    pucks: list[Puck]
+    score: int
+
+    def __init__(self, name: str, puck_count: int, color: tuple[int, int, int, int]):
+        self.name = name
+        self.total_pucks = puck_count
+        self.left_pucks = puck_count
+        self.pucks = []
+        self.color = color
+        self.score = 0
+
+
+PLAYER_COLOR_LIST: list[tuple[int, int, int]] = [  # BGRA
+    (255, 0, 0, 255),  # blue
+    (0, 0, 255, 255),  # red
+    (0, 255, 0, 255),  # green
+    (0, 255, 255, 255),  # yellow
+]
 
 
 class Shuffleboard:
     def __init__(
         self,
-        fps: int,
         board_size: tuple[int, int],
         puck_radius: int,
         players: int = 2,
@@ -55,11 +63,11 @@ class Shuffleboard:
     ):
         self.board_size = board_size  # 보드 크기 (w, h) (mm단위)
         self.puck_radius = puck_radius  # 퍽의 반지름
-        self.player_list = [Player(i, f"P{i+1}", 0, PUCK_COLORS[i]) for i in range(np.clip(players, 2, 4))]  # 플레이어 정보
+        # self.player_list = [Player(i, f"P{i+1}", 0, PUCK_COLORS[i]) for i in range(np.clip(players, 2, 4))]  # 플레이어 정보
         self.pucks_per_player = pucks_per_player  # 플레이어 당 퍽 개수
+        self.player_list = [PlayerData(f"P{i+1}", pucks_per_player, PLAYER_COLOR_LIST[i]) for i in range(np.clip(players, 2, 4))]
 
         self.current_player_idx: int = 0  # 현재 플레이어 인덱스
-        self.placed_puck_list: list[Optional[Puck]] = []  # 배치된 퍽 정보
         self.current_puck: Optional[Puck] = None
 
         # Pymunk
@@ -82,10 +90,8 @@ class Shuffleboard:
         """퍽의 위치를 기반으로 점수를 업데이트한다."""
         for player in self.player_list:
             player.score = 0
-        for puck in self.placed_puck_list:
-            if puck is None:
-                continue
-            puck.owner.score += self.getScoreByPosition(puck)
+            for puck in player.pucks:
+                player.score += self.getScoreByPosition(puck)
 
     def getScoreByPosition(self, puck: Puck) -> int:
         """위치에 따른 점수를 반환한다."""
@@ -104,7 +110,7 @@ class Shuffleboard:
 
         return 0
 
-    def placePuck(self, position: Optional[tuple[int, int]] = None, player: Optional[Player] = None):
+    def placePuck(self, position: Optional[tuple[int, int]] = None, player: Optional[PlayerData] = None):
         """게임 보드에 퍽을 배치한다."""
         if player is None:
             player = self.getCurrentPlayer()
@@ -115,8 +121,9 @@ class Shuffleboard:
         body.position = position
         shape = pymunk.Circle(body, self.puck_radius)
         self.space.add(body, shape)
-        self.placed_puck_list.append(Puck(player, body, shape))
-        self.current_puck = self.placed_puck_list[-1]
+
+        self.current_puck = Puck(body, shape)
+        player.pucks.append(self.current_puck)
 
     def hitPuck(self, puck: Puck, velocity: tuple[int, int]):
         """퍽의 속도를 직접 설정한다."""
@@ -128,15 +135,15 @@ class Shuffleboard:
             return
         self.hitPuck(self.current_puck, velocity)
 
-    def getPlacedPucks(self):
+    def getPlacedPucks(self) -> list[Puck]:
         """배치된 퍽들을 반환한다."""
-        return [puck for puck in self.placed_puck_list if puck is not None]
+        return self.placed_puck_list
 
     def stepSpace(self, dt: int):
         """시뮬레이션 공간에서 시간의 흐름을 진행한다."""
         self.space.step(dt / 1000.0)
 
-    def getCurrentPlayer(self):
+    def getCurrentPlayer(self) -> PlayerData:
         """현재 플레이어를 반환한다."""
         return self.player_list[self.current_player_idx]
 
@@ -181,35 +188,34 @@ class Shuffleboard:
         x, y = offset
         w, h = self.board_size
 
-        color = (238, 238, 238, 255)
+        line_color = (238, 238, 238, 255)
         # 모서리 경계선
-        cv2.line(img, (x, y), (x + w, y), color, 2)  # 위
-        cv2.line(img, (x, y + h), (x + w, y + h), color, 2)  # 아래
-        cv2.line(img, (x, y), (x, y + h), color, 2)  # 좌
-        cv2.line(img, (x + w, y), (x + w, y + h), color, 2)  # 우
+        cv2.line(img, (x, y), (x + w, y), line_color, 2)  # 위
+        cv2.line(img, (x, y + h), (x + w, y + h), line_color, 2)  # 아래
+        cv2.line(img, (x, y), (x, y + h), line_color, 2)  # 좌
+        cv2.line(img, (x + w, y), (x + w, y + h), line_color, 2)  # 우
         # 점수 경계선
         prev_area = 0.0
         for score_area in SCORE_AREA_LIST:
             prev_area += score_area.ratio
             _x = int(x + prev_area * w)
-            cv2.line(img, (_x, y), (_x, y + h), color, 2)
+            cv2.line(img, (_x, y), (_x, y + h), line_color, 2)
 
     def drawPucks(self, img: np.ndarray, offset: tuple[int, int]):
         """게임 보드에 퍽을 그린다."""
         x, y = offset
-        for puck in self.placed_puck_list:
-            if puck is None:
-                continue
-            cx, cy = puck.body.position
-            center = (int(cx) + x, int(cy) + y)
-            color = (*puck.owner.puck_color, 255)
-            cv2.circle(img, center, self.puck_radius, color, -1)
+
+        for player in self.player_list:
+            for puck in player.pucks:
+                cx, cy = puck.body.position
+                center = (int(cx) + x, int(cy) + y)
+                cv2.circle(img, center, self.puck_radius, player.color, -1)
 
 
 def main():
     fps = 12
     w, h = (25 * 12, 25 * 5)
-    game = Shuffleboard(fps=fps, board_size=(w, h), puck_radius=10)
+    game = Shuffleboard(board_size=(w, h), puck_radius=10)
 
     dt = 1000 // fps
     while True:
