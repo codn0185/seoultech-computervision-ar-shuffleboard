c14d1b067b8c634585b0854cfa2b9c59b074251e
codn0185 <113341200+codn0185@users.noreply.github.com>
2026-06-05 03:56:41 +0900

    chore: game_view 내 그리기 메서드 호출 순서 변경

--- src/ar_shuffleboard/views/game_view.py
+++ src/ar_shuffleboard/views/game_view.py
@@ -18,24 +18,6 @@ class GameView:
 
     # === Game Canvas ===
 
-    def drawGameBoarderLines(self, image: np.ndarray):
-        """게임 보드 경계선 그리기"""
-        x, y = self.getGameOrigin()
-        w, h = self.board_size
-
-        line_color = (238, 238, 238, 255)
-        # 모서리 경계선
-        cv2.line(image, (x, y), (x + w, y), line_color, 2)  # 위
-        cv2.line(image, (x, y + h), (x + w, y + h), line_color, 2)  # 아래
-        cv2.line(image, (x, y), (x, y + h), line_color, 2)  # 좌
-        cv2.line(image, (x + w, y), (x + w, y + h), line_color, 2)  # 우
-        # 점수 경계선
-        prev_area = 0.0
-        for score_area in SCORE_AREA_LIST:
-            prev_area += score_area.ratio
-            _x = int(x + prev_area * w)
-            cv2.line(image, (_x, y), (_x, y + h), line_color, 2)
-
     def drawScoreArea(self, img: np.ndarray):
         """점수 영역 그리기"""
         x, y = self.getGameOrigin()
@@ -48,6 +30,24 @@ class GameView:
             x2 = int(x + prev_area * w)
             color = score_area.color
             cv2.rectangle(img, (x1, y), (x2, y + h), color, -1)
+
+    def drawGameBoarderLines(self, image: np.ndarray):
+        """게임 보드 경계선 그리기"""
+        x, y = self.getGameOrigin()
+        w, h = self.board_size
+
+        line_color = (0, 0, 0, 255)  # black
+        # 모서리 경계선
+        cv2.line(image, (x, y), (x + w, y), line_color, 2)  # 위
+        cv2.line(image, (x, y + h), (x + w, y + h), line_color, 2)  # 아래
+        cv2.line(image, (x, y), (x, y + h), line_color, 2)  # 좌
+        cv2.line(image, (x + w, y), (x + w, y + h), line_color, 2)  # 우
+        # 점수 경계선
+        prev_area = 0.0
+        for score_area in SCORE_AREA_LIST:
+            prev_area += score_area.ratio
+            _x = int(x + prev_area * w)
+            cv2.line(image, (_x, y), (_x, y + h), line_color, 2)
 
     def drawPucks(self, image: np.ndarray, players: list[PlayerData]):
         """게임 퍽 그리기"""
@@ -62,8 +62,8 @@ class GameView:
         """게임 캔버스 반환"""
         w, h = self.board_size
         canvas: np.ndarray = np.zeros((GAME_BOARD_SCALE * h, GAME_BOARD_SCALE * w, 4), dtype=np.uint8)
-        self.drawGameBoarderLines(canvas)
         self.drawScoreArea(canvas)
+        self.drawGameBoarderLines(canvas)
         self.drawPucks(canvas, players)
         return canvas
 
